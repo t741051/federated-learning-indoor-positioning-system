9611e3be2bafe420a45e86123743f96e209b684e
t741051 <t741051@gmail.com>
2023-05-23 20:03:24 +0800

    remove client val func, add --partition option

---
+++ client_3Devices.py
@@ -0,0 +1,170 @@
+import argparse
+import os
+from pathlib import Path
+import numpy as np
+import tensorflow as tf
+import flwr as fl
+from keras.applications.mobilenet import MobileNet
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+
+# Make TensorFlow logs less verbose
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+# Define Flower client
+
+
+class CifarClient(fl.client.NumPyClient):
+
+    def __init__(self, model, train_generator):
+        self.model = model
+        self.train_generator = train_generator
+
+    def get_properties(self, config):
+        """Get properties of client."""
+        raise Exception("Not implemented")
+
+    def get_parameters(self, config):
+        """Get parameters of the local model."""
+        raise Exception(
+            "Not implemented (server-side parameter initialization)")
+
+    def fit(self, parameters, config):
+        """Train parameters on the locally held training set."""
+
+        # Update local model parameters
+        self.model.set_weights(parameters)
+
+        # Get hyperparameters for this round
+        batch_size: int = config["batch_size"]
+        epochs: int = config["local_epochs"]
+
+        history = self.model.fit(
+            self.train_generator,
+            batch_size=batch_size,
+            epochs=epochs,
+        )
+
+        # Return updated model parameters and results
+        parameters_prime = self.model.get_weights()
+        num_examples_train = len(self.train_generator[0])
+
+        results = {
+            "loss": history.history["loss"][0],
+            "accuracy": history.history["accuracy"][0],
+            # "val_loss": history.history["val_loss"][0],
+            # "val_accuracy": history.history["val_accuracy"][0],
+        }
+        return parameters_prime, num_examples_train, results
+
+    # def evaluate(self, parameters, config):
+    #     """Evaluate parameters on the locally held test set."""
+
+    #     # Update local model with global parameters
+    #     self.model.set_weights(parameters)
+
+    #     # Get config values
+    #     # steps: int = config["val_steps"]
+
+    #     # Evaluate global model parameters on the local test data and return results
+    #     # loss, accuracy = self.model.evaluate(
+    #     #     self.x_test, self.y_test, 32, steps=steps)
+
+    #     loss, accuracy = self.model.evaluate(
+    #         self.val_generator, steps=len(self.val_generator))
+
+    #     # num_examples_test = len(self.x_test)
+    #     num_examples_test = len(self.val_generator)
+    #     return loss, num_examples_test, {"accuracy": accuracy}
+
+
+def main() -> None:
+    # Parse command line argument `partition`
+    parser = argparse.ArgumentParser(description="Flower")
+    parser.add_argument(
+        "--partition",
+        type=int,
+        default=0,
+        choices=range(0, 10),
+        required=True,
+        help="Specifies the artificial data partition of CIFAR10 to be used. "
+        "Picks partition 0 by default",
+    )
+
+    args = parser.parse_args()
+
+    # Load and compile Keras model
+
+    base_model = MobileNet(input_shape=(
+        75, 75, 3), include_top=False, weights='imagenet')
+
+    # 添加自定義的輸出層
+    x = base_model.output
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dense(1024, activation='relu')(x)
+    predictions = tf.keras.layers.Dense(17, activation='softmax')(x)
+
+    # 構建完整模型
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # 鎖定 InceptionResNetV2 的卷積層權重，只訓練自定義的輸出層
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Load a subset of CIFAR-10 to simulate the local data partition
+    # (x_train, y_train), (x_test, y_test) = load_partition(args.partition)
+
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,  # 对图像进行缩放，将像素值归一化到[0,1]之间
+        rotation_range=30,  # 随机旋转角度范围
+        width_shift_range=0.2,  # 随机水平平移的范围
+        height_shift_range=0.2,  # 随机竖直平移的范围
+        shear_range=0.1,  # 剪切强度
+        zoom_range=0.2,  # 随机缩放范围
+        horizontal_flip=False,  # 随机水平翻转
+        fill_mode='nearest'  # 填充方式
+    )
+    if (args.partition == 0):
+        train_dir = 'Dataset_3Devices/Nokia/train/'
+    elif (args.partition == 1):
+        train_dir = 'Dataset_3Devices/HTC/train/'
+    elif (args.partition == 2):
+        train_dir = 'Dataset_3Devices/Mi/train/'
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(75, 75),
+        batch_size=32,
+        class_mode='categorical'
+    )
+
+    # Start Flower client
+
+    client = CifarClient(model, train_generator)
+
+    fl.client.start_numpy_client(
+        server_address="127.0.0.1:8080",
+        # server_address="140.118.122.247:8080",
+        client=client,
+        root_certificates=Path(".cache/certificates/ca.crt").read_bytes(),
+    )
+
+
+def load_partition(idx: int):
+    """Load 1/10th of the training and test data to simulate a partition."""
+    assert idx in range(10)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    return (
+        x_train[idx * 5000: (idx + 1) * 5000],
+        y_train[idx * 5000: (idx + 1) * 5000],
+    ), (
+        x_test[idx * 1000: (idx + 1) * 1000],
+        y_test[idx * 1000: (idx + 1) * 1000],
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ server_3Devices.py
@@ -0,0 +1,134 @@
+from typing import Dict, Optional, Tuple
+from pathlib import Path
+import flwr as fl
+import numpy as np
+import tensorflow as tf
+from keras.applications.mobilenet import MobileNet
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+
+
+def main() -> None:
+    # Load and compile model for
+    # 1. server-side parameter initialization
+    # 2. server-side parameter evaluation
+
+    base_model = MobileNet(input_shape=(
+        75, 75, 3), include_top=False, weights='imagenet')
+
+    # 添加自定義的輸出層
+    x = base_model.output
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dense(1024, activation='relu')(x)
+    predictions = tf.keras.layers.Dense(17, activation='softmax')(x)
+
+    # 構建完整模型
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # 鎖定 InceptionResNetV2 的卷積層權重，只訓練自定義的輸出層
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Create strategy
+    # strategy = fl.server.strategy.FedAvg(
+    #     fraction_fit=0.3,
+    #     # fraction_evaluate=0.2,
+    #     min_fit_clients=3,
+    #     # min_evaluate_clients=1,
+    #     min_available_clients=3,
+    #     evaluate_fn=get_evaluate_fn(model),
+    #     on_fit_config_fn=fit_config,
+    #     # on_evaluate_config_fn=evaluate_config,
+    #     initial_parameters=fl.common.ndarrays_to_parameters(
+    #         model.get_weights()),
+    # )
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=0.3,
+        min_fit_clients=3,
+        min_available_clients=3,
+        evaluate_fn=get_evaluate_fn(model),
+        on_fit_config_fn=fit_config,
+        initial_parameters=fl.common.ndarrays_to_parameters(
+            model.get_weights()),
+    )
+
+    # Start Flower server (SSL-enabled) for four rounds of federated learning
+    fl.server.start_server(
+        server_address="0.0.0.0:8080",
+        config=fl.server.ServerConfig(num_rounds=10),
+        strategy=strategy,
+        certificates=(
+            Path(".cache/certificates/ca.crt").read_bytes(),
+            Path(".cache/certificates/server.pem").read_bytes(),
+            Path(".cache/certificates/server.key").read_bytes(),
+        ),
+    )
+
+
+def get_evaluate_fn(model):
+    """Return an evaluation function for server-side evaluation."""
+
+    # Load data and model here to avoid the overhead of doing it in `evaluate` itself
+
+    # 创建验证集的 ImageDataGenerator 对象
+    validation_datagen = ImageDataGenerator(rescale=1./255)
+    val_dir = 'Dataset_3Devices/val/'
+
+    # 加载验证集
+    validation_generator = validation_datagen.flow_from_directory(
+        val_dir,
+        target_size=(75, 75),
+        batch_size=32,
+        class_mode='categorical'
+    )
+    # x_val = np.load('val_image.npy')
+    # y_val = np.load('val_label.npy')
+    # The `evaluate` function will be called after every round
+
+    def evaluate(
+        server_round: int,
+        parameters: fl.common.NDArrays,
+        config: Dict[str, fl.common.Scalar],
+    ) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
+        # Update model with the latest parameters
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(validation_generator)
+        best_accuracy = -np.inf  # 賦初值
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            model.save("model.h5")
+        return loss, {"accuracy": accuracy}
+
+    return evaluate
+
+
+def fit_config(server_round: int):
+    """Return training configuration dict for each round.
+
+    Keep batch size fixed at 32, perform two rounds of training with one
+    local epoch, increase to two local epochs afterwards.
+    """
+    config = {
+        "batch_size": 32,
+        "local_epochs": 1 if server_round < 2 else 2,
+    }
+    return config
+
+
+def evaluate_config(server_round: int):
+    """Return evaluation configuration dict for each round.
+
+    Perform five local evaluation steps on each client (i.e., use five
+    batches) during rounds one to three, then increase to ten local
+    evaluation steps.
+    """
+    val_steps = 5 if server_round < 4 else 10
+    return {"val_steps": val_steps}
+
+
+if __name__ == "__main__":
+    main()
